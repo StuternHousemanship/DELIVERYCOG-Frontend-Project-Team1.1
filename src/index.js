0da92d1c34c835d500960d825ee45bb42f0e81ba
mueez sanwo <mueezsanwo@gmail.com>
2022-10-20 17:40:41 +0100

    Mueez T1: DYC-1 Onboarding Features V3

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { createBrowserHistory } from "history";
+
+export const history = createBrowserHistory();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter history={history} >
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want your app to work offline and load faster, you can change
