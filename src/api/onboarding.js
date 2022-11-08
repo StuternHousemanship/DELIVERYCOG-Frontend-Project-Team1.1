ac4bd8de694fae0740be03e2dac57c93259a2017
Iyinoludiya <102791210+Iyinoludiya@users.noreply.github.com>
2022-11-09 00:39:01 +0100

    Iyin T3: DYC-1 Onboarding Features V11

--- src/api/onboarding.js
+++ src/api/onboarding.js
@@ -1,12 +1,16 @@
 /* eslint-disable import/no-cycle */
+// import Login from "pages/logIn";
 import { deliverycogOnboarding } from "../config";
 
 export default {
   /** Send a POST request to Sign Up Users */
-  async SignUp(firstName, lastName) {
+  async SignUp(firstName, lastName, email, phoneNumber, password) {
     const data = {
       firstName,
       lastName,
+      email,
+      phoneNumber,
+      password,
     };
     const stringifiedData = JSON.stringify(data);
 
@@ -21,5 +25,16 @@ export default {
     const stringifiedData = JSON.stringify(data);
 
     return deliverycogOnboarding.post("/Verify/OtpCode", stringifiedData);
+  },
+
+  /** Send a POST request to Login users */
+  async Login(email, password) {
+    const data = {
+      email,
+      password,
+    };
+    const stringifiedData = JSON.stringify(data);
+
+    return deliverycogOnboarding.post("/users/login", stringifiedData);
   },
 };
