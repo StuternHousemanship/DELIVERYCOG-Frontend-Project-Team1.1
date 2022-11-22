c4c0e25d835c192e7e1e367365e91ce0d67d10c7
kolade07 <abisoyejudah001@gmail.com>
2022-11-22 23:50:01 +0100

    Abisoye T3: DYC-1 Onboarding Features V18

--- src/pages/forgotPassword.js
+++ src/pages/forgotPassword.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { NonAuthRoutes } from "../url";
+import forgotImage from "../assets/Images/HandsGive.png";
 import Header from "../header";
 
 function ForgotPassword() {
@@ -19,115 +20,33 @@ function ForgotPassword() {
   /* displays Forgot Password Layout */
   const forgotPasswordLayout = () => {
     return (
-      <div className="hidden md:block lg:block">
-        <Header />
-        <div className="flex justify-center items-center">
-          <div className="my-[71px] h-[490px] w-[609px] bg-white">
-            <p className="mt-12 font-Inter font-[700] text-4xl text-black text-center">
-              Forgot Password?
-            </p>
-            <p className="text-center font-sans font-[500] text-[#898989] mt-[24px]">
-              Enter the email used for Delivery COG. <br /> we will send you a
-              link to reset reset your password
-            </p>
-            <form>
-              <div className="mt-6 mx-20  w-[450]">
-                <label className="" htmlFor="email">
-                  <p className="mb-2 text-base font-sans font-[400] text-deliverycog-grey-text-color">
-                    Email
-                  </p>
-                  <input
-                    id="reset-email"
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                    placeholder="example@gmail.com"
-                    className="py-2 px-2 font-sans font-[600] text-deliverycog-grey-text-color text-base h-14 w-full border rounded border-[#717171] appearance-none focus:outline-none"
-                  />
-                </label>
-              </div>
-              <div className="mt-[74px] mx-20 w-[450] ">
-                <button
-                  type="button"
-                  title="Continue"
-                  onClick={() => handleForgotPasswordButton()}
-                  className="py-2 px-2 font-sans font-[600] bg-[#16D176] text-deliverycog-white-text-color text-base h-14 w-full border rounded border-[#717171] appearance-none focus:outline-none"
-                >
-                  <a href=" ">Continue</a>
-                </button>
-              </div>
-              <div className="mt-6 mx-20 w-[450] ">
-                <button
-                  type="button"
-                  title="Continue"
-                  className="py-2 px-2 font-sans font-[600] text-[#16D176] bg-[#ffffff] hover:bg-[#8AE8BA] active:bg-[#EBF6F0] text-base h-14 w-full border rounded border-[#717171] appearance-none focus:outline-none"
-                  onClick={() => navigate(NonAuthRoutes.logIn)}
-                >
-                  Cancel
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    );
-  };
+            <><p className="text-[#424242] text-[16px] sm:font-[650] leading-[16px] mb-[6px] ">
+        Email
+      </p><input
+          type="email"
+          id="mail"
+          placeholder="example@gmail.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          className=" p-3 sm:w-[450px] xs:w-[348px] h-[56px] rounded-[4px] border-[1px] border-[#717171] outline-0  " /><div>
+          <button
+            type="submit"
+            className=" xs:w-[348px] sm:w-[450px]  h-[56px] rounded-[4px] border-[1px] border-[#717171] outline-0 bg-[#16D176] text-white font-sans text-[20px] leading-[23px] font-[400] absolute sm:top-[365px] sm:left-[80px] xs:top-[655px] xs:left-[40px] mb-2"
+            onClick={() => handleForgotPasswordButton()}
+          >
+            Continue
+          </button>
 
-  // eslint-disable-next-line no-unused-vars
-  const forgotPasswordLayoutMobile = () => {
-    return (
-      <div className="md:block lg:block">
-        <Header />
-        <div className="flex justify-center items-center">
-          <div className="my-[71px] h-[490px] w-[609px] bg-white">
-            <p className="mt-12 font-Inter font-[700] text-4xl text-black text-center">
-              Forgot Password?
-            </p>
-            <p className="text-center font-sans font-[500] text-[#898989] mt-[24px]">
-              Enter the email used for Delivery COG. <br /> we will send you a
-              link to reset reset your password
-            </p>
-            <form>
-              <div className="mt-6 mx-20  w-[450]">
-                <label className="" htmlFor="email">
-                  <p className="mb-2 text-base font-sans font-[400] text-deliverycog-grey-text-color">
-                    Email
-                  </p>
-                  <input
-                    id="reset-email"
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                    placeholder="example@gmail.com"
-                    className="py-2 px-2 font-sans font-[600] text-deliverycog-grey-text-color text-base h-14 w-full border rounded border-[#717171] appearance-none focus:outline-none"
-                  />
-                </label>
-              </div>
-              <div className="mt-[74px] mx-20 w-[450] ">
-                <button
-                  type="button"
-                  title="Continue"
-                  onClick={() => handleForgotPasswordButton()}
-                  className="py-2 px-2 font-sans font-[600] bg-[#16D176] text-deliverycog-white-text-color text-base h-14 w-full border rounded border-[#717171] appearance-none focus:outline-none"
-                >
-                  <a href=" ">Continue</a>
-                </button>
-              </div>
-              <div className="mt-6 mx-20 w-[450] ">
-                <button
-                  type="button"
-                  title="Continue"
-                  className="py-2 px-2 font-sans font-[600] text-[#16D176] bg-[#ffffff] hover:bg-[#8AE8BA] active:bg-[#EBF6F0] text-base h-14 w-full border rounded border-[#717171] appearance-none focus:outline-none"
-                  onClick={() => navigate(NonAuthRoutes.logIn)}
-                >
-                  Cancel
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
+          <button
+            type="submit"
+            className=" xs:w-[348px] sm:w-[450px]  h-[56px] rounded-[5px] border-[2px] border-[#16D176] outline-0 text-black font-sans text-[20px] leading-[23px] font-[400] absolute sm:top-[435px] sm:left-[80px] xs:top-[735px] xs:left-[40px] "
+            onClick={() => handleForgotPasswordButton()}
+          >
+            Cancel
+          </button>
+        </div></>
+        </form>
       </div>
     );
   };
@@ -135,24 +54,39 @@ function ForgotPassword() {
   /* displays Forgot Password Success Layout */
   const forgotPasswordSuccessLayout = () => {
     return (
-      <div className="sm:h-[100vh] sm:w-screen sm:bg-[#E5E5E5] flex justify-center items-center xs:w-[428px] xs:h-[900px] xs:bg-white  ">
-        <div className="bg-white sm:w-[609px] sm:h-[438px] xs:w-full xs:h-full flex justify-center  relative">
-          <div className="absolute sm:top-[48px] sm:left-[80px] xs:top-[335px] xs:left-[45px] mb-[40px] ">
-            <h1 className="text-[#424242] font-Inter font-[700] text-[36px] leading-[44px] ">
-              Forgot password
+      <div className="sm:h-[100vh] sm:w-screen sm:bg-[#FFFFFF] flex justify-center items-center xs:w-[428px] xs:h-[900px] xs:bg-white  ">
+        <div className="bg-white rounded-[5px] border-[2px] border-[#16D176] sm:w-[600px] sm:h-[650px] xs:w-full xs:h-full relative">
+          <div className="w-[100%] flex justify-center">
+            <img
+              src={forgotImage}
+              alt="forgotImage"
+              className="w-[200px] h-[180px] -mt-0.98 "
+            />
+          </div>
+          <div className="sm:top-[4px] sm:left-[80px] xs:top-[335px] xs:left-[45px] mb-[40px]">
+            <h1 className="text-[#424242] font-Inter font-[700] text-[30px] sm:top-[470px] leading-[44px] mt-5 xs:top-[680px] text-center">
+              Check Your Inbox
             </h1>
           </div>
-          <p className="text-[#424242] xs:w-[349px] sm:w-[418px] text-[16px] leading-[19.36px] absolute sm:top-[149px] sm:left-[80px] xs:top-[411px] xs:left-[45px] mb-[40px] ">
-            Kindly go to your email inbox or spam folder to confirm and change
-            your password
+          <p className="text-[#424242] text-center xs:w-[349px] sm:w-[435px] text-[17px] leading-[19.36px] absolute sm:top-[280px] sm:left-[80px] xs:top-[500px] xs:left-[45px] mb-[40px] ">
+            We just sent alog in link to name@email.com. The link will expire
+            shortly.
           </p>
           <button
             type="submit"
-            className=" xs:w-[338px] sm:w-[450px] xs:h-[42px] sm:h-[56px] rounded-[4px] border-[1px] border-[#717171] outline-0 bg-[#424242] text-white font-sans text-[20px] leading-[23px] font-[400] absolute sm:top-[238px] sm:left-[80px] xs:top-[505px] xs:left-[45px] "
+            className=" xs:w-[338px] sm:w-[450px] xs:h-[42px] sm:h-[56px] rounded-[4px] border-[1px] outline-0 bg-[#16D176] text-white font-sans text-[20px] leading-[23px] font-[400] absolute sm:top-[420px] sm:left-[80px] xs:top-[720px] xs:left-[45px] "
             onClick={() => navigate(NonAuthRoutes.logIn)}
           >
-            Verify
+            Resend email
           </button>
+
+          <p className="text-[#424242] xs:w-[349px] sm:w-[418px] text-[17px] text-center leading-[19.36px] absolute sm:top-[500px] sm:left-[80px] xs:top-[750px] xs:left-[45px] mb-[40px] ">
+            Can&apos;t find your link. Check your spam folder.
+          </p>
+
+          <p className="text-[#424242] xs:w-[349px] sm:w-[418px] text-[16px] leading-[19.36px] absolute sm:top-[560px] sm:left-[80px] xs:top-[760px] xs:left-[45px] mb-[40px] text-center ">
+            Go back
+          </p>
         </div>
       </div>
     );
