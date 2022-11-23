a3bc53d449af52936a189f21dac04911edd7a1cd
Chukwuebuka <Chukwuebuka.tech@gmail.com>
2022-11-23 13:15:49 +0100

    Chukwuebuka: Project Setup

---
+++ src/components/footer/onboardingFooter.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { ReactComponent as MaskDelivery } from "../../assets/svg/hand-mask-delivery-icon.svg";
+import { ReactComponent as BikeDelivery } from "../../assets/svg/bike-delivery-icon.svg";
+
+function Footer() {
+  /** global footerIcons */
+  /* eslint no-undef: "error" */
+  const footerIcons = () => {
+    return (
+      <div className="sticky top-0 z-50 md:hidden">
+        <MaskDelivery className="fixed bottom-0 left-0" />
+        <BikeDelivery className="fixed bottom-0 right-0" />
+      </div>
+    );
+  };
+  return <div>{footerIcons()}</div>;
+}
+
+export default Footer;
