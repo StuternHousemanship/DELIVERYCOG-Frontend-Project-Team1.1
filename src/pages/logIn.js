156374012b4279bc83917ce3fa4582156a91a101
Iyinoludiya <102791210+Iyinoludiya@users.noreply.github.com>
2022-12-04 14:39:17 +0100

    Iyin T3: DYC-1 Onboarding Features V65

--- src/pages/logIn.js
+++ src/pages/logIn.js
@@ -81,231 +81,232 @@ function LogIn() {
       //   // navigate(NonAuthRoutes.logOutErrorLayout);
       // }
     }
+  };
 
-    const logInLargeScreen = () => {
-      return (
-        <div className="hidden md:block lg:block">
-          <nav className="fixed h-0 w-full">
-            <Header />
-          </nav>
-          <div className="flex justify-center items-center">
-            <div className="my-[71px] h-532px w-[609px] bg-white">
-              <p className="mt-12 ml-60 font-Inter font-[700] text-3xl text-black">
-                Log in
-              </p>
-              <form onSubmit={() => handleLogin()}>
-                <div className="mt-6 mx-20  w-[450]">
-                  <label className="" htmlFor="email">
-                    <p className="mb-2 text-base font-sans font-[400] text-deliverycog-grey-text-color">
-                      Email
-                    </p>
-                    <input
-                      id="log-email"
-                      type="email"
-                      value={email}
-                      placeholder="Enter your email address"
-                      pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
-                      className="py-2 px-2 font-sans font-[600] text-deliverycog-grey-text-color text-base h-14 w-full border rounded border-[#d9d6d6] hover:border-[#16D176] appearance-none focus:outline-none"
-                      onChange={() => validateEmail(e.target.value)}
-                    />
-                  </label>
-                </div>
-                {isEmailValid ? null : displayEmailErrorText()}
-                <div className="mt-6 mx-20 w-[450]">
-                  <label className="" htmlFor="password">
-                    <p className="mb-2 text-base font-sans font-[400] text-deliverycog-grey-text-color">
-                      Password
-                    </p>
-                    <input
-                      id="log-password"
-                      type={showPassword ? "text" : "password"}
-                      pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                      placeholder="Enter your password"
-                      className="py-2 px-2 font-sans font-[600] text-deliverycog-grey-text-color text-base h-14 w-full border rounded border-[#d9d6d6] hover:border-[#16D176] appearance-none focus:outline-none"
-                      onChange={() => setPassword(e.target.value)}
-                    />
-                    <span>
-                      {showPassword ? (
-                        <PasswordShow
-                          className="absolute -mt-10 pr-2 ml-[420px]"
-                          onClick={() => handleShowPassword()}
-                        />
-                      ) : (
-                        <PasswordHide
-                          className="absolute -mt-10 pr-2 ml-[420px]"
-                          onClick={() => handleShowPassword()}
-                        />
-                      )}
-                    </span>
-                  </label>
-                </div>
-                <div className="mt-3 mb-10 w-[450] text-right underline mr-20">
+  const logInLargeScreen = () => {
+    return (
+      <div className="hidden md:block lg:block">
+        <nav className="fixed h-0 w-full">
+          <Header />
+        </nav>
+        <div className="flex justify-center items-center">
+          <div className="my-[71px] h-532px w-[609px] bg-white">
+            <p className="mt-12 ml-60 font-Inter font-[700] text-3xl text-black">
+              Log in
+            </p>
+            <form onSubmit={() => handleLogin()}>
+              <div className="mt-6 mx-20  w-[450]">
+                <label className="" htmlFor="email">
+                  <p className="mb-2 text-base font-sans font-[400] text-deliverycog-grey-text-color">
+                    Email
+                  </p>
+                  <input
+                    id="log-email"
+                    type="email"
+                    value={email}
+                    placeholder="Enter your email address"
+                    pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+                    className="py-2 px-2 font-sans font-[600] text-deliverycog-grey-text-color text-base h-14 w-full border rounded border-[#d9d6d6] hover:border-[#16D176] appearance-none focus:outline-none"
+                    onChange={(e) => validateEmail(e.target.value)}
+                  />
+                </label>
+              </div>
+              {isEmailValid ? null : displayEmailErrorText()}
+              <div className="mt-6 mx-20 w-[450]">
+                <label className="" htmlFor="password">
+                  <p className="mb-2 text-base font-sans font-[400] text-deliverycog-grey-text-color">
+                    Password
+                  </p>
+                  <input
+                    id="log-password"
+                    type={showPassword ? "text" : "password"}
+                    pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                    placeholder="Enter your password"
+                    className="py-2 px-2 font-sans font-[600] text-deliverycog-grey-text-color text-base h-14 w-full border rounded border-[#d9d6d6] hover:border-[#16D176] appearance-none focus:outline-none"
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <span>
+                    {showPassword ? (
+                      <PasswordShow
+                        className="absolute -mt-10 pr-2 ml-[420px]"
+                        onClick={() => handleShowPassword()}
+                      />
+                    ) : (
+                      <PasswordHide
+                        className="absolute -mt-10 pr-2 ml-[420px]"
+                        onClick={() => handleShowPassword()}
+                      />
+                    )}
+                  </span>
+                </label>
+              </div>
+              <div className="mt-3 mb-10 w-[450] text-right underline mr-20">
+                <button
+                  type="button"
+                  title="Forgot password"
+                  onClick={() => {
+                    navigate(NonAuthRoutes.forgotPassword);
+                  }}
+                >
+                  Forgot password
+                </button>
+              </div>
+              <div className="mt-6 mx-20 w-[450]">
+                <button
+                  type="submit"
+                  title="submit"
+                  onClick={() => handleLogin()}
+                  className="py-2 px-2 font-sans font-[600] bg-[#16D176] hover:bg-[#3DD98D] active:bg-[#12AE62] text-deliverycog-white-text-color text-base h-14 w-full border rounded border-[#16D176] appearance-none focus:outline-none"
+                >
+                  Continue
+                </button>
+                <div className="mt-6 mx- w-[450] ">
                   <button
-                    type="button"
-                    title="Forgot password"
-                    onClick={() => {
-                      navigate(NonAuthRoutes.forgotPassword);
-                    }}
-                  >
-                    Forgot password
-                  </button>
-                </div>
-                <div className="mt-6 mx-20 w-[450]">
-                  <button
+                    id="cancelButton-web"
                     type="submit"
-                    title="submit"
-                    onClick={() => handleLogin()}
-                    className="py-2 px-2 font-sans font-[600] bg-[#16D176] hover:bg-[#3DD98D] active:bg-[#12AE62] text-deliverycog-white-text-color text-base h-14 w-full border rounded border-[#16D176] appearance-none focus:outline-none"
-                  >
-                    Continue
-                  </button>
-                  <div className="mt-6 mx- w-[450] ">
-                    <button
-                      id="cancelButton-web"
-                      type="submit"
-                      onClick={() => navigate(NonAuthRoutes.landingPage)}
-                      className="py-2 px-2 font-sans font-[600] text-[#16D176] bg-[#ffffff] hover:bg-[#8AE8BA] active:bg-[#EBF6F0] text-base h-14 w-full border rounded border-[#16D176] appearance-none focus:outline-none"
-                    >
-                      Cancel
-                    </button>
-                  </div>
-                  <button
-                    id="signup-button"
-                    type="submit"
-                    onClick={() => {
-                      navigate(NonAuthRoutes.signUp);
-                    }}
-                    className="mt-6 mb-10 mx-24 text-center"
-                  >
-                    Do not have an account?
-                    <span className="text-[#16D176]"> Sign up</span>
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-          <div className="mt-10">
-            <OnboardingFooter />
-          </div>
-        </div>
-      );
-    };
-
-    const logInMobile = () => {
-      return (
-        <div className="md:hidden lg:hidden w-full">
-          <nav className="fixed h-2 w-full">
-            {" "}
-            <Header />
-          </nav>
-          <div className="flex justify-center items-center">
-            <div className="h-[716px] w-[390px] bg-white">
-              <p className="mt-[120px] ml-6 font-Inter font-[700] text-2xl text-black">
-                Log in
-              </p>
-              <form onSubmit={() => handleLogin()}>
-                <div className="mt-6 mx-6 w-[342]">
-                  <label className="" htmlFor="email">
-                    <p className="mb-2 text-base font-sans font-[400] text-deliverycog-grey-text-color">
-                      Email
-                    </p>
-                    <input
-                      id="mobile-mail"
-                      type="email"
-                      placeholder="Enter your email"
-                      onChange={() => setEmail(e.target.value)}
-                      className="py-2 px-2 font-sans font-[600] text-deliverycog-grey-text-color text-base h-14 w-full border rounded border-[#d9d6d6] hover:border-[#16D176]  appearance-none focus:outline-none"
-
-                      // onKeyUp={(e) => validateEmail(e.target.value)}
-                    />
-                  </label>
-                  {isEmailValid ? null : displayEmailErrorText()}
-                </div>
-                <div className="mt-6 mx-6 w-[342]">
-                  <label className="" htmlFor="password">
-                    <p className="mb-2 text-base font-sans font-[400] text-deliverycog-grey-text-color">
-                      Password
-                    </p>
-                    <input
-                      id="mobile-pass"
-                      type={showPassword ? "text" : "password"}
-                      pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                      placeholder="Enter your password"
-                      value={password}
-                      onChange={() => setPassword(e.target.value)}
-                      className="py-2 px-2 font-sans font-[600] text-deliverycog-grey-text-color text-base h-14 w-full border rounded border-[#d9d6d6] hover:border-[#16D176] appearance-none focus:outline-none"
-                    />
-                    <span className="absolute inset-y-[1667/100] mr-9">
-                      {showPassword ? (
-                        <PasswordShow
-                          className="absolute -ml-7 pr-2 mt-4 "
-                          onClick={() => handleShowPassword()}
-                        />
-                      ) : (
-                        <PasswordHide
-                          className="absolute -ml-7 pr-2 mt-4 "
-                          onClick={() => handleShowPassword()}
-                        />
-                      )}
-                    </span>
-                  </label>
-                </div>
-
-                <div className="mt-3 mb-10 w-[342] text-right text-sm underline mr-7">
-                  <button
-                    type="button"
-                    title="Forgot"
-                    onClick={() => {
-                      navigate(NonAuthRoutes.forgotPassword);
-                    }}
-                  >
-                    Forgot password
-                  </button>
-                </div>
-                <div className="mt-6 mx-6 w-[342] ">
-                  <button
-                    type="submit"
-                    title="Continue"
-                    onClick={() => handleLogin()}
-                    className="py-2 px-2 font-sans font-[600] bg-[#16D176] hover:bg-[#3DD98D] active:bg-[#12AE62] text-deliverycog-white-text-color text-base h-14 w-full border rounded border-[#16D176] appearance-none focus:outline-none"
-                  >
-                    Continue
-                  </button>
-                </div>
-                <div className="mt-6 mx-6 w-[342] ">
-                  <button
-                    id="cancel-button-mobile"
-                    type="submit"
-                    onClick={() => navigate(NonAuthRoutes.logIn)}
+                    onClick={() => navigate(NonAuthRoutes.landingPage)}
                     className="py-2 px-2 font-sans font-[600] text-[#16D176] bg-[#ffffff] hover:bg-[#8AE8BA] active:bg-[#EBF6F0] text-base h-14 w-full border rounded border-[#16D176] appearance-none focus:outline-none"
                   >
                     Cancel
                   </button>
-                  <button
-                    id="signup-mobile-button"
-                    type="submit"
-                    onClick={() => {
-                      navigate(NonAuthRoutes.signUp);
-                    }}
-                    className="mt-6 mx-14  w-[342] text-center text-sm"
-                  >
-                    Do not have an account?
-                    <span className="text-[#16D176]"> Sign Up</span>
-                  </button>
                 </div>
-              </form>
-            </div>
+                <button
+                  id="signup-button"
+                  type="submit"
+                  onClick={() => {
+                    navigate(NonAuthRoutes.signUp);
+                  }}
+                  className="mt-6 mb-10 mx-24 text-center"
+                >
+                  Do not have an account?
+                  <span className="text-[#16D176]"> Sign up</span>
+                </button>
+              </div>
+            </form>
           </div>
         </div>
-      );
-    };
-
-    return (
-      <div>
-        {logInLargeScreen()}
-        {logInMobile()}
+        <div className="mt-10">
+          <OnboardingFooter />
+        </div>
       </div>
     );
   };
+
+  const logInMobile = () => {
+    return (
+      <div className="md:hidden lg:hidden w-full">
+        <nav className="fixed h-2 w-full">
+          {" "}
+          <Header />
+        </nav>
+        <div className="flex justify-center items-center">
+          <div className="h-[716px] w-[390px] bg-white">
+            <p className="mt-[120px] ml-6 font-Inter font-[700] text-2xl text-black">
+              Log in
+            </p>
+            <form onSubmit={() => handleLogin()}>
+              <div className="mt-6 mx-6 w-[342]">
+                <label className="" htmlFor="email">
+                  <p className="mb-2 text-base font-sans font-[400] text-deliverycog-grey-text-color">
+                    Email
+                  </p>
+                  <input
+                    id="mobile-mail"
+                    type="email"
+                    placeholder="Enter your email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="py-2 px-2 font-sans font-[600] text-deliverycog-grey-text-color text-base h-14 w-full border rounded border-[#d9d6d6] hover:border-[#16D176]  appearance-none focus:outline-none"
+
+                    // onKeyUp={(e) => validateEmail(e.target.value)}
+                  />
+                </label>
+                {isEmailValid ? null : displayEmailErrorText()}
+              </div>
+              <div className="mt-6 mx-6 w-[342]">
+                <label className="" htmlFor="password">
+                  <p className="mb-2 text-base font-sans font-[400] text-deliverycog-grey-text-color">
+                    Password
+                  </p>
+                  <input
+                    id="mobile-pass"
+                    type={showPassword ? "text" : "password"}
+                    pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="py-2 px-2 font-sans font-[600] text-deliverycog-grey-text-color text-base h-14 w-full border rounded border-[#d9d6d6] hover:border-[#16D176] appearance-none focus:outline-none"
+                  />
+                  <span className="absolute inset-y-[1667/100] mr-9">
+                    {showPassword ? (
+                      <PasswordShow
+                        className="absolute -ml-7 pr-2 mt-4 "
+                        onClick={() => handleShowPassword()}
+                      />
+                    ) : (
+                      <PasswordHide
+                        className="absolute -ml-7 pr-2 mt-4 "
+                        onClick={() => handleShowPassword()}
+                      />
+                    )}
+                  </span>
+                </label>
+              </div>
+
+              <div className="mt-3 mb-10 w-[342] text-right text-sm underline mr-7">
+                <button
+                  type="button"
+                  title="Forgot"
+                  onClick={() => {
+                    navigate(NonAuthRoutes.forgotPassword);
+                  }}
+                >
+                  Forgot password
+                </button>
+              </div>
+              <div className="mt-6 mx-6 w-[342] ">
+                <button
+                  type="submit"
+                  title="Continue"
+                  onClick={() => handleLogin()}
+                  className="py-2 px-2 font-sans font-[600] bg-[#16D176] hover:bg-[#3DD98D] active:bg-[#12AE62] text-deliverycog-white-text-color text-base h-14 w-full border rounded border-[#16D176] appearance-none focus:outline-none"
+                >
+                  Continue
+                </button>
+              </div>
+              <div className="mt-6 mx-6 w-[342] ">
+                <button
+                  id="cancel-button-mobile"
+                  type="submit"
+                  onClick={() => navigate(NonAuthRoutes.logIn)}
+                  className="py-2 px-2 font-sans font-[600] text-[#16D176] bg-[#ffffff] hover:bg-[#8AE8BA] active:bg-[#EBF6F0] text-base h-14 w-full border rounded border-[#16D176] appearance-none focus:outline-none"
+                >
+                  Cancel
+                </button>
+                <button
+                  id="signup-mobile-button"
+                  type="submit"
+                  onClick={() => {
+                    navigate(NonAuthRoutes.signUp);
+                  }}
+                  className="mt-6 mx-14  w-[342] text-center text-sm"
+                >
+                  Do not have an account?
+                  <span className="text-[#16D176]"> Sign Up</span>
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      {logInLargeScreen()}
+      {logInMobile()}
+    </div>
+  );
 }
+
 export default LogIn;
